114583caaf479d021490c45342000e5c2f171e40
dustinmaiden <dustinmaiden@gmail.com>
2017-05-17 16:55:41 -0500

    Adding files
    Magic Squares project from Machine Organization.
    Compile and run from terminal

---
+++ generate_magic.c
@@ -0,0 +1,197 @@
+//////////////////////////////////////////
+// Main File : generate_magic.c		//
+// Semester  : CS 354 Fall 2016 001	//
+//					//
+// Author    : Dustin Maiden		//
+// Email     : dmaiden@wisc.edu		//
+// CS Login  : maiden			//
+//					//	
+///////////SOURCES OF HELP////////////////
+//I have experience working on and 	//
+//with magic squares before and have    //
+//been taught the modular arithmetic    //
+// 					//
+//Unlike most student in CS 354, I have //
+//multiple undergraduate degrees and    //
+//CS experience. I believe we went over //
+//modular arithmetic in my disctete math//
+//course at UW - Whitewater. If finding //
+//the instructor and course information //
+// is necessary, I am happy to generate //
+// that information			//
+/////////////////////////////////////////
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Structure representing Square
+// size: dimension(number of rows/columns) of the square
+// array: 2D array of integers
+typedef struct _Square {
+	int size;
+	int **array;
+} Square;
+
+/* Get function to obtain desired square size from user
+ *
+ * Returns integer, which is the row and column height  
+*/
+int get_square_size();
+
+/* generates a magic square 
+ *
+ * size - the number of rows and columns
+ * filename - filename for generating square 
+*/
+void generate_magic(int size, char *filename);
+
+/* Write the magic square to a file
+ *
+ * square - magic square to write to file
+ * filename - filename to create/edit
+ */
+void write_to_file(Square * square, char *filename);
+
+/* Main Function
+ *
+ * 1. Obtain filename to create/edit
+ * 2. Obtain size of magic square from user
+ * 3. Generate magic square
+ * 4. Write square to fil5
+ */
+int main(int argc, char *argv[])
+{
+
+	//error handling - too many and too few CLAs	
+        if(argc>2){
+        printf("Usage : ./verify_magic <filename>\n");
+        exit(1);
+        }
+        if(argc<2){
+        printf("Usage : ./verify_magic <filename>\n");
+        exit(1);
+        }
+
+	// Check input arguments to get filename
+	char *OutputFile=argv[1];
+	
+	// Get size from user, validate for parameters >=3 and odd
+	int size = get_square_size();
+	if(size <3 || size%2 ==0){
+		printf("Size must be an odd number >= 3.\n");
+		exit(1);
+	}
+
+	// Generate the magic square	
+        generate_magic(size,OutputFile);
+
+	
+	return 0;
+}
+
+// get_square_size prompts the user for the magic square size
+// checks if it is an odd number >= 3 and returns the number
+int get_square_size()
+{
+	int input;
+	printf("Enter size of magic square, must be odd \n");
+	scanf("%d",&input);
+	return input;
+}
+
+// generate_magic constructs a magic square of size given from the user
+// construction using the Siamese algorithm and returns the Square struct
+void generate_magic(int n, char *filename )
+{
+	char *ProgramFile = filename;
+	
+	//struct and memory allocation
+	Square square = {n};
+	square.array=(int **)malloc(n*sizeof(int *));
+
+	int x;
+	for (x=0;x<n;x++){
+	square.array[x] = (int *)malloc(n*sizeof(int));
+	}
+	square.size=n;
+
+
+	int fullsize = n*n; //total # of int's in matrix
+	int row = 0;        // start at top row
+	int col = n/2;      // start in the middle column
+	int index;
+
+	for (index=1; index<=fullsize; ++index){
+	//pointer to pointer notation    
+	//place integer starting from 1 going to n*n
+    		*(*(square.array+col)+row)=index;  
+    		row--; // move down a row
+    		col++; // move up a col
+
+	//magic squares are easily generated using modular arithmetic
+	//to generate the square, when the n-th integer is a mutliple
+	// of n, set it at the desired location 
+	// utilizing the "up and to the right" method = r-- && col++
+		int modulo=index%n;
+    		if (modulo == 0) { 
+      			row += 2; 
+      			-- col; 
+    		}
+    		else {
+			//if you are on the columns edge, need to back up
+			if (col == n) 
+        		col -= n;
+      		else 	//if you are off of the matrix for a row, move to bottom
+			if (row < 0) 
+        		row += n;
+    		}		
+  	}
+
+
+	write_to_file(&square,ProgramFile);
+	
+	//memory de-allocation
+	int y;
+	for (y=0;y<n;y++){
+		free(square.array[y]);
+	}
+	free(square.array);
+
+
+}
+
+// write_to_file opens up a new file(or overwrites the existing file)
+// and writes out the square in the format expected by verify_magic.c
+void write_to_file(Square * square, char *filename)
+{
+
+	char *ProgramFile = filename;
+	FILE *output = fopen(ProgramFile,"w");
+
+	//if the file can be opened
+	//write magic square to file
+	if(output){	
+
+	//write row size (=column size)
+	fprintf(output,"%d ", square->size);
+	fprintf(output,"\n");
+	int n = square->size;	
+		
+	//print magic square to file
+	int a,b;
+        for (a=0; a<n; a++){
+        	for (b=0; b<n; b++){
+        	fprintf(output,"%d", *(*(square->array+a)+b));
+        		if(b<n-1)
+			fprintf(output,",");     
+        	}	
+		fprintf(output,"\n");
+	}	
+	fclose(output);
+
+	}
+	else{
+	printf("Cannot open file for reading.\n");
+	exit(1);
+	}
+}
---
+++ verify_magic.c
@@ -0,0 +1,217 @@
+/////////////////////////////////////////////////////////
+// Main File:	verify_magic.c
+// Semester:	CS 354 Fall 2016 Lecture 001
+//
+// Author:	Dustin Maiden
+// Email	dmaiden@wisc.edu
+// CS Login	maiden
+//
+/////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Structure representing Square
+typedef struct _Square {
+	int size;
+	int **array;
+} Square;
+
+
+/* Read input from file and Build Potential Magic Square
+ * filename - file to read formatted integer matrix
+ *
+ * NOTE - the above return is what should be utilized for 
+ * the project specifications. Unfortunately, I am struggling
+ * passing these structs around and will call verify_magic
+ * from within construct_square - thus negating the need 
+ * for the return 
+ */ 
+void construct_square(char *filename);
+
+
+/* Verify if integer matrix is a magic square or not
+ * square - integer matrix to be checked for magic square
+ * Returns an integer denoting if the test is true or false 
+ */
+int verify_magic(Square * square);
+
+
+/* xxxxxxx MAIN FUNCTION xxxxxxxxx */ 
+int main(int argc, char *argv[])
+{
+	//error handling - too many and too few CLAs
+	if(argc>2){
+	printf("Usage : ./generate_magic <filename>\n");
+	exit(1);
+	}
+	if(argc<2){
+	printf("Usage : ./generate_magic <filename>\n");
+	exit(1);
+	}
+
+
+	// Check input arguments to get filename
+	char *ProgramFile=argv[1];
+	FILE *m_square = fopen(ProgramFile,"r");
+
+	//If the file can be opened
+	//Contruct Square -> Verify Square	
+	if(m_square){	
+	construct_square(ProgramFile);
+	}
+	
+	// if the file cannot be opened
+	else{
+	printf("File cannot be opened\n");
+	exit(1);	
+	}
+
+	return 0;
+}
+
+
+/* construct_square reads the input file to initialize a square struct
+// from the contents of the file and then passes this struct to be 
+// verified whether it is a magic square or not*/
+void construct_square(char *filename)
+{
+	char *ProgramFile = filename;
+	FILE *square = fopen(ProgramFile,"r");
+	
+	int n;
+	int q;
+	
+	//Begin reading file for matrix size
+	fscanf(square,"%d",&n);
+//	printf("Number of rows and columns: %i",n);
+	
+ 	// Initialize a new Square struct of that size	
+   	Square Square = {n};	
+	Square.array=(int **)malloc(n*sizeof(int *));
+	
+	int i;
+	for(i=0; i<n; i++)
+		Square.array[i] = (int *)malloc(n*sizeof(int));
+	
+	//Read formatted integers and create matrix using pointer notation
+	int a,b;
+	for (a=0; a<n; a++){
+          	for (b=0; b<n; b++){	
+			fscanf(square,"%d,",&q);
+			*(*(Square.array+a)+b)=q;
+//			printf(" %d ",*(*(Square.array+a)+b));
+			}
+		}
+
+	fclose(square);
+
+	//verify the struct just created
+	int verify_test =  verify_magic(&Square);
+		if(verify_test == 1){
+			printf("false\n");
+			exit(1);
+		}
+		else if(verify_test == 2){
+			printf("true\n");
+		}
+
+	//memory de allocation
+	int y;
+	for(y=0; y<n; y++)
+		free(Square.array[y]);
+	free(Square.array);
+	
+}
+
+// verify_magic verifies if the square is a magic square
+// returns 1(true) or 0(false)
+// a magic square has the sum of each row equal to the sum of each column
+// and equal to the sum of the main and opposite diaganol
+
+int verify_magic(Square * square)
+{
+
+	int rows = square->size;
+
+	//a bunch of counters to use to calculate
+	// row sums, column sums, and diagonal sums
+	// using for loops
+	int k;
+	int i,j;
+	int l,m;
+	int o,p;
+	int q,r;
+	
+
+ 	//find magic number
+	int magicNum=0;
+	for(k=0;k<rows;k++){
+		magicNum+= *(*(square->array+0)+k);
+	}
+
+	//find sum of each row
+	for(i=0;i<rows;i++){
+		int tempSum=0;
+		for(j=0;j<rows;j++){
+			tempSum += *(*(square->array+i)+j);
+		}
+
+		if(tempSum == magicNum){
+		//the sum of the row equals the magic number	
+		}	
+		else {	return 1;	
+		}
+	
+	}		
+	
+	//find sum of each column
+	for(l=0;l<rows;l++){
+		int tempSum2=0;
+		for(m=0;m<rows;m++){
+			tempSum2 += *(*(square->array+m)+l);
+		}
+	
+		if(tempSum2 == magicNum){
+		//the sum of the column equals the magic number			
+		}
+		else {return 1;
+		}
+
+	}
+
+	// find sum of main diaganol
+	int tempSum3 = 0;
+	for(o=0;o<rows;o++){	
+		for(p=0;p<rows;p++){
+			if(o == p){
+				tempSum3 += *(*(square->array+o)+p);
+			}
+		}
+	}	
+
+	if(tempSum3 == magicNum){
+	//the main diagonal equals the magic number
+	}
+	else {return 1;
+	}
+
+	// Check secondary diagonal
+	int tempSum4 = 0;
+	for(q=rows-1;q>-1;q--){	
+		for(r=rows-1;r>-1;r--){
+			if(q == r){
+				tempSum4 += *(*(square->array+q)+r);	
+			}
+		}
+	}
+	
+	if(tempSum4 == magicNum){
+	//the opposite diagonal equals the magic number
+	}
+	else {return 1;
+	}
+
+	return 2;
+}
